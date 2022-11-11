92ea6c42332310ea96baf3cc3d497b907edbae72
Wallas Curty <65136877+WallasCurty@users.noreply.github.com>
2022-11-11 09:12:13 -0300

    Alteração no Alert do JS

--- js/calc_imc.js
+++ js/calc_imc.js
@@ -1,50 +1,47 @@
 function calcular() {
     var formulario = document.getElementById("formulario");
 
-    var peso = +formulario.peso.value;
-    var altura = +formulario.altura.value;
-    var feminino =+ formulario.feminino.value;
-    var masculino =+ formulario.masculino.value;
+    var peso = formulario.peso.value;
+    var altura = formulario.altura.value;
+    var feminino = formulario.feminino.value;
+    var masculino = formulario.masculino.value;
 
     var imc = peso / (altura * altura);
 
-    formulario.imc.value = imc.toFixed(2);
     if(masculino){
         if (imc < 19.1) {
-            alert('Você esta abaixo do peso!');
+            alert('Você esta abaixo do peso! Seu IMC é igual a ' + imc.toFixed(2));
         }
         else if (imc <= 25.8) {
-            alert("Peso Ideal");
+            alert("Peso Ideal! Seu IMC é igual a " + imc.toFixed(2));
         }
         else if (imc <= 27.3) {
-            alert("Sobrepeso");
+            alert("Sobrepeso! Seu IMC é igual a " + imc.toFixed(2));
         }
         else if (imc <= 32.3) {
-            alert("Obesidade Moderada");
+            alert("Obesidade Moderada! Seu IMC é igual a " + imc.toFixed(2));
         }
         else if (imc > 32.3){
-            alert('Gordo');
+            alert("Gordo!  Seu IMC é igual a " + imc.toFixed(2) );
         }
     
     }else if(feminino){
         if (imc < 20.7) {
-            alert('Você esta abaixo do peso!');
+            alert('Você esta abaixo do peso! Seu IMC é igual a ' + imc.toFixed(2));
         }
         else if (imc <= 26.4) {
-            alert("Peso Ideal");
+            alert("Peso Ideal! Seu IMC é igual a " + imc.toFixed(2));
         }
         else if (imc <= 27.8) {
-            alert("Sobrepeso");
+            alert("Sobrepeso! Seu IMC é igual a " + imc.toFixed(2));
         }
         else if (imc <= 31.1) {
-            alert("Obesidade Moderada");
+            alert("Obesidade Moderada! Seu IMC é igual a " + imc.toFixed(2));
         }
         else if (imc > 31.1){
-            alert('Gordo');
+            alert("Gordo!  Seu IMC é igual a " + imc.toFixed(2) );
         }
         
-    }else{
-        alert('Você ainda não digitou nada')
     }
     
 }
